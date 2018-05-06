6427e634972c2e2ebdbe9e3773fe90bb1a2086ba
Matt Lewis <matt_lewis@live.co.uk>
2018-05-06 12:39:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Lazy Load Transcript</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
