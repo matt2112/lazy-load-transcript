b2f2349352ce12551481cb14459995cb6e5c001a
Matt Lewis <matt_lewis@live.co.uk>
2018-05-07 22:39:02 +0100

    fill in some tests and update flow-typed folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import App from './App';
 
-it('should mount without exploding', () => {
-  const wrapper = mount(<App />);
+import { NEXT, ENGLISH, FRENCH } from './constants';
+
+// Full mount would be better but still need to figure out best way to
+// mock axios call on mount.
+it('should render without exploding', () => {
+  const wrapper = shallow(<App />);
   expect(wrapper).toMatchSnapshot();
 });
 
@@ -15,10 +19,32 @@ it('should retrieve data on initial render', () => {
   expect(spy).toHaveBeenCalledTimes(1);
 });
 
-it('should change page in state when changePage method is called', () => {});
+it('should change page in state when changePage method is called', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().currentPage).toBe(0);
+  wrapper.instance().changePage(NEXT);
+  expect(wrapper.state().currentPage).toBe(0);
+  wrapper.setState({
+    pages: {
+      english: [['one two three'], ['one two three']],
+      french: []
+    }
+  });
+  wrapper.instance().changePage(NEXT);
+  expect(wrapper.state().currentPage).toBe(1);
+});
 
-it('should make a call to the Google API when translate button is clicked', () => {});
-
-it('should only show translate button when translated text is stored in state', () => {});
-
-it('should switch view when the language is switched from English to French and back', () => {});
+it('should switch view when the language is switched from English to French and back', () => {
+  const wrapper = shallow(<App />);
+  wrapper.setState({
+    pages: {
+      english: [['one two three'], ['one two three']],
+      french: [['un deux trois'], ['un deux trois']]
+    }
+  });
+  expect(wrapper.state().currentLanguage).toBe(ENGLISH);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.instance().changeLanguage(FRENCH);
+  expect(wrapper.state().currentLanguage).toBe(FRENCH);
+  expect(wrapper).toMatchSnapshot();
+});
